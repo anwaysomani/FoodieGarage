6084132f8481960b1ca6e076ecf8d98b3d824292
Anway <anwaysomani@gmail.com>
2019-11-03 23:48:58 +0530

    Create model for restaurant

--- foodiegarage/restaurants/models.py
+++ foodiegarage/restaurants/models.py
@@ -1,6 +1,29 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class restaurant(models.Model):
+    restaurant_id = models.IntegerField(unique=True, primary_key=True)
+    restaurant_name = models.CharField(max_length=200)
+    cuisines = models.TextField()
+    average_cost_for_two = models.IntegerField()
+    currency = models.CharField(max_length=120)
+    has_table_booking = models.BooleanField()
+    has_online_delivery = models.BooleanField()
+    aggregate_rating = models.DecimalField(max_digits=2, decimal_places=1)
+    rating_color = models.CharField(max_length=50)
+    rating_text = models.CharField(max_length=45)
+    votes = models.IntegerField()
+    country_code = models.IntegerField()
+    city = models.CharField(max_length=120)
+    address = models.TextField()
+    locality = models.TextField()
+    locality_verbose = models.TextField()
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.restaurant_name
+
+    class Meta:
+        verbose_name = "Restaurant"
+        verbose_name_plural = "Restaurants"
